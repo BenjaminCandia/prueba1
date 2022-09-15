b9faec1ceeafc2c6b1ed4076c043c48f7f0cf697
bencant <bencant@DESKTOP-CAV9BGG>
2022-09-15 02:02:16 -0300

    creacion de clases

---
+++ src/atlas/Atlas.java
@@ -0,0 +1,24 @@
+
+package atlas;
+
+import atlasDTO.club_deportivo;
+import atlasDTO.suscripcion;
+import atlasDTO.usuarios;
+
+public class Atlas {
+
+    public static void main(String[] args) {
+        usuarios u = new usuarios();
+        u.setId(0);
+        u.setRut(0);
+        u.setDv();
+        u.setNombre_completo(nombre_completo);
+        u.setNombre_usuario(nombre_usuario);
+        u.setContraseña(contraseña);
+        u.setEmail(email);
+        u.setFecha(fecha);
+        u.setTelefono(0);
+               
+    }
+    
+}
