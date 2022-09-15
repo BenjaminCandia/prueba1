366565bb5a6cc63915a9f09ca689521aec71e8fd
bencant <bencant@DESKTOP-CAV9BGG>
2022-09-15 03:34:40 -0300

    creacion de clases suscripcion

--- src/atlas/Atlas.java
+++ src/atlas/Atlas.java
@@ -12,15 +12,53 @@ public class Atlas {
         usuarios u = new usuarios();
         u.setId(1010);
         u.setRut(12345678);
-        u.setDv("");
-        u.setNombre_completo("");
-        u.setNombre_usuario("");
-        u.setContraseña("");
-        u.setEmail("");
-        u.setFecha(new Date());
-        u.setTelefono(0);
+        u.setDv("5");
+        u.setNombre_completo("benjamin alberto candia toro");
+        u.setNombre_usuario("bencant");
+        u.setContraseña("loquillo123");
+        u.setEmail("bencant@duoc.cl");
+        u.setFecha_nac("29/06/2003");
+        u.setTelefono(987651234);
         
-               
+        System.out.println("ID: "+u.getId());
+        System.out.println("RUT: "+u.getRut()+"-"+u.getDv());
+        System.out.println("NOMBRE COMPLETO: "+u.getNombre_completo());
+        System.out.println("NOMBRE DE USUARIO: "+u.getNombre_usuario());
+        System.out.println("CONTRASEÑA"+u.getContraseña());
+        System.out.println("GMAIL: "+u.getEmail());
+        System.out.println("FECHA DE NACIMIENTO: "+u.getFecha_nac());
+        System.out.println("TELEFONO: "+u.getTelefono());
+        
+        System.out.println("=============================");
+        
+        club_deportivo cd =new club_deportivo();
+        cd.setCodigo(0);
+        cd.setColores("azul, negro");
+        cd.setFecha_origen("1999");
+        cd.setLema("EL QUE SE RINDE ES WECO");
+        cd.setNombre_club("LOS PULENTOS");
+        cd.setNombre_fundador("BENJITA FOR THE WIN");
+        cd.setPais_origen("CHILE");
+        cd.setValor_sub(10000);
+        
+        System.out.println("CODIGO: "+cd.getCodigo());
+        System.out.println("NOMBRE DEL FUNDADOR: "+cd.getNombre_fundador());
+        System.out.println("NOMBRE DEL CLUB: "+cd.getNombre_club());
+        System.out.println("LEMA: "+cd.getLema());
+        System.out.println("PAIS DE ORIGEN: "+cd.getPais_origen());
+        System.out.println("VALOR DE SUBCRIPCION: $"+cd.getValor_sub());
+        System.out.println("COLOR: "+cd.getColores());
+        System.out.println("AÑO DE FUNDACION: "+cd.getFecha_origen());
+        
+        System.out.println("=============================");
+        
+        
+        
+        
+        
+        
+        
+        
     }
     
 }
--- src/atlasDTO/club_deportivo.java
+++ src/atlasDTO/club_deportivo.java
@@ -8,7 +8,7 @@ public class club_deportivo
     private int codigo;
     private String nombre_club;
     private String nombre_fundador;
-    private Date fecha;
+    private String fecha_origen;
     private String pais_origen;
     private String lema;
     private String colores; 
@@ -18,18 +18,18 @@ public class club_deportivo
         this.codigo = 0;
         this.nombre_club = "";
         this.nombre_fundador = "";
-        this.fecha = new Date();
+        this.fecha_origen = "";
         this.pais_origen = "";
         this.lema = "";
         this.colores = "";
         this.valor_sub = 0;
     }
 
-    public club_deportivo(int codigo, String nombre_club, String nombre_fundador, Date fecha, String pais_origen, String lema, String colores, int valor_sub) {
+    public club_deportivo(int codigo, String nombre_club, String nombre_fundador, String fecha_origen, String pais_origen, String lema, String colores, int valor_sub) {
         this.codigo = codigo;
         this.nombre_club = nombre_club;
         this.nombre_fundador = nombre_fundador;
-        this.fecha = fecha;
+        this.fecha_origen = fecha_origen;
         this.pais_origen = pais_origen;
         this.lema = lema;
         this.colores = colores;
@@ -60,12 +60,12 @@ public class club_deportivo
         this.nombre_fundador = nombre_fundador;
     }
 
-    public Date getFecha() {
-        return fecha;
+    public String getFecha_origen() {
+        return fecha_origen;
     }
 
-    public void setFecha(Date fecha) {
-        this.fecha = fecha;
+    public void setFecha_origen(String fecha_origen) {
+        this.fecha_origen = fecha_origen;
     }
 
     public String getPais_origen() {
@@ -99,10 +99,5 @@ public class club_deportivo
     public void setValor_sub(int valor_sub) {
         this.valor_sub = valor_sub;
     }
-    
-    
-            
-            
-            
-            
-}
+
+}    
